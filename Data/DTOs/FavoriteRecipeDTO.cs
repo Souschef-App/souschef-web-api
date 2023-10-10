e738ff5acf11db974649f1b672042c3239ff9bef
Aryan480 <aryanmwadi1@outlook.com>
2023-10-10 19:03:34 -0400

    verify DateTime

--- Data/DTOs/FavoriteRecipeDTO.cs
+++ Data/DTOs/FavoriteRecipeDTO.cs
@@ -3,7 +3,7 @@ public class FavoriteRecipeDto
     public int Id { get; set; }
     public int UserId { get; set; }
     public int RecipeId { get; set; }
-    public DateTime DateAdded { get; set; }
+    public DateTime DateAdded { get; set; } //check this
     public RecipeDto Recipe { get; set; }
     public UserDto User { get; set; }
 }
