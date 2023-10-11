4e7757eb901263ae86eed42921aac9106f5c86cd
Aryan480 <aryanmwadi1@outlook.com>
2023-10-10 20:10:29 -0400

    DELETED interfaces

--- Data/Repository/Contracts/IMealPlanRepository.cs
+++
@@ -1,8 +0,0 @@
-public interface IMealPlanRepository
-{
-    Task<MealPlan> GetMealPlanAsync(int mealPlanId);
-    Task<IEnumerable<MealPlan>> GetAllMealPlansAsync();
-    Task AddMealPlanAsync(MealPlan mealPlan);
-    Task UpdateMealPlanAsync(int mealPlanId, MealPlan updatedMealPlan);
-    Task DeleteMealPlanAsync(int mealPlanId);
-}
