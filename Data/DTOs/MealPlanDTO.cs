1153cfd0c31df5185ff9791f19429f673d299ab5
Aryan480 <aryanmwadi1@outlook.com>
2023-10-10 20:11:21 -0400

    refined Meal Plan DTO

--- Data/DTOs/MealPlanDTO.cs
+++ Data/DTOs/MealPlanDTO.cs
@@ -1,11 +1,5 @@
-public class MealDto
+public class MealPlanDto
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public List<RecipeDto> Recipes { get; set; }
-}
-public class UpdateMealPlanDto
-{
-    public string Name { get; set; }
     public DateTime Date { get; set; }
 }
