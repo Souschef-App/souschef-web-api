97331b41ff52e2a4baa6ffa5001070f976bc897e
Aryan480 <aryanmwadi1@outlook.com>
2023-10-10 20:07:25 -0400

    modified FavoriterecipeDTO

--- Data/DTOs/FavoriteRecipeDTO.cs
+++ Data/DTOs/FavoriteRecipeDTO.cs
@@ -1,9 +1,8 @@
 public class FavoriteRecipeDto
 {
     public int Id { get; set; }
-    public int UserId { get; set; }
-    public int RecipeId { get; set; }
-    public DateTime DateAdded { get; set; } //check this
-    public RecipeDto Recipe { get; set; }
-    public UserDto User { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsFavorite { get; set; }
+    // Add other properties as needed
 }
