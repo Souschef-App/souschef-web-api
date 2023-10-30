131855205310c9a4cbeda30fb9fc8b153eacb004
Aryan480 <aryanmwadi1@outlook.com>
2023-10-29 21:08:38 -0400

    modified and fixed controllers

--- Controllers/MealSessionController.cs
+++ Controllers/MealSessionController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using souschef.server.Data.DTOs;
+using souschef.server.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,14 +23,16 @@ public class MealSessionController : ControllerBase
         var sessionDtos = sessions.Select(session => new MealSessionDto
         {
             Id = session.Id,
-            DateTime = session.DateTime
+            DateTime = session.DateTime,
+            Users = session.Users,
+            Plan = session.Plan
         }).ToList();
 
         return Ok(sessionDtos);
     }
 
     [HttpGet("{id}")]
-    public ActionResult<MealSessionDto> GetSession(int id)
+    public ActionResult<MealSessionDto> GetSession(Guid id)
     {
         var session = _repository.Get(id);
         if (session == null)
@@ -37,30 +41,33 @@ public class MealSessionController : ControllerBase
         var sessionDto = new MealSessionDto
         {
             Id = session.Id,
-            DateTime = session.DateTime
+            DateTime = session.DateTime,
             // Map other properties here
+            Users = session.Users,
+            Plan = session.Plan
         };
 
         return Ok(sessionDto);
     }
 
-    [HttpPost]
-    public ActionResult<MealSessionDto> CreateSession(MealSession model)
+    [HttpPost()]
+    public ActionResult<MealSessionDto> CreateSession(MealSessionCreateDTO model)
     {
         var createdSession = _repository.Create(model);
 
         var sessionDto = new MealSessionDto
         {
             Id = createdSession.Id,
-            DateTime = createdSession.DateTime
-            
+            DateTime = createdSession.DateTime,
+            Users = createdSession.Users,
+            Plan = createdSession.Plan
         };
 
         return CreatedAtAction(nameof(GetSession), new { id = sessionDto.Id }, sessionDto);
     }
 
     [HttpPut("{id}")]
-    public IActionResult UpdateSession(int id, MealSession model)
+    public IActionResult UpdateSession(Guid id, MealSessionCreateDTO model)
     {
         var existingSession = _repository.Get(id);
         if (existingSession == null)
@@ -73,7 +80,7 @@ public class MealSessionController : ControllerBase
     }
 
     [HttpDelete("{id}")]
-    public IActionResult DeleteSession(int id)
+    public IActionResult DeleteSession(Guid id)
     {
         var session = _repository.Get(id);
         if (session == null)
@@ -81,5 +88,25 @@ public class MealSessionController : ControllerBase
 
         _repository.Delete(id);
         return NoContent();
+    }
+
+    [HttpGet("{id}/users")]
+    public ActionResult<ApplicationUser> GetSessionUsers(Guid id)
+    {
+        return Ok(_repository.Get(id).Users);
+    }
+
+    [HttpPost("{id}/users/{userId}")]
+    public ActionResult<ApplicationUser> CreateSessionUsers(Guid id, Guid userId)
+    {
+        _repository.CreateSessionUser(id, userId);
+        return Ok();
+    }
+
+    [HttpDelete("{id}/users/{userId}")]
+    public ActionResult<ApplicationUser> DeleteSessionUsers(Guid id, Guid userId)
+    {
+        _repository.DeleteSessionUser(id, userId);
+        return Ok();
     }
 }
