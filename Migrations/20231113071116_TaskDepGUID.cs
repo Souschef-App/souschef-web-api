e157ee66783473750d09a0df65a542ce8814273b
sethclim <sethclim@gmail.com>
2023-11-13 02:12:03 -0500

    migration for GUID dep list on task

---
+++ Migrations/20231113071116_TaskDepGUID.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace souschef.server.Migrations
+{
+    public partial class TaskDepGUID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid[]>(
+                name: "Dependencies",
+                table: "Tasks",
+                type: "uuid[]",
+                nullable: true,
+                oldClrType: typeof(int[]),
+                oldType: "integer[]",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int[]>(
+                name: "Dependencies",
+                table: "Tasks",
+                type: "integer[]",
+                nullable: true,
+                oldClrType: typeof(Guid[]),
+                oldType: "uuid[]",
+                oldNullable: true);
+        }
+    }
+}
