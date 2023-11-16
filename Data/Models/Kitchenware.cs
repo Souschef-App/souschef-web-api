5ea8a7b58ea58f1a4f85ef68906f06709b47c82a
sethclim <sethclim@gmail.com>
2023-11-16 17:31:30 -0500

    updated kitchenware

--- Data/Models/Kitchenware.cs
+++ Data/Models/Kitchenware.cs
@@ -8,7 +8,7 @@ namespace souschef.server.Data.Models
         [Key]
         public Guid Id { get; set; }
 
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Quantity { get; set; }
     }
 }
